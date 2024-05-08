15f1a1adbf779aab4960cc021771db0b4feb1c45
nada <nadahamed723@gmail.com>
2024-05-08 10:50:13 +0300

    project main features completed

--- GoShop/Modules/SignUpScreen/ViewModel/SignUpViewModel.swift
+++ GoShop/Modules/SignUpScreen/ViewModel/SignUpViewModel.swift
@@ -61,18 +61,23 @@ class SignUpViewModel{
     func isValidEmail(_ email: String) -> Bool {
         let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
         let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        print("\(emailPredicate.evaluate(with: email))")
         return emailPredicate.evaluate(with: email)
     }
 
     func isValidPassword(_ password: String) -> Bool {
         let passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$"
         let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
+        print("\(passwordPredicate.evaluate(with: password))")
+
         return passwordPredicate.evaluate(with: password)
     }
     
     func isValidPhoneNumber(_ phoneNumber: String) -> Bool {
         let phoneRegex = #"^\+?[0-9]{1,}$"#
         let phonePredicate = NSPredicate(format: "SELF MATCHES %@", phoneRegex)
+        print("\(phonePredicate.evaluate(with: phoneNumber))")
+
         return phonePredicate.evaluate(with: phoneNumber)
     }
     
